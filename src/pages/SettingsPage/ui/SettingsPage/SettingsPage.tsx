b32f66f0601dba4d3fc696183cd5ad93486de43d
Xenia.Liubachka <bygermina@gmail.com>
2025-02-27 22:23:04 +0100

    add theme to settings

--- src/pages/SettingsPage/ui/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/ui/SettingsPage/SettingsPage.tsx
@@ -2,7 +2,8 @@ import { useTranslation } from 'react-i18next';
 
 import { Text } from '@/shared/ui/redesigned/Text';
 import { Page } from '@/widgets/Page';
-import { VStack } from '@/shared/ui/redesigned/Stack';
+import { HStack, VStack } from '@/shared/ui/redesigned/Stack';
+import { ThemeSwitcher } from '@/features/ThemeSwitcher';
 
 const SettingsPage = () => {
     const { t } = useTranslation();
@@ -11,6 +12,10 @@ const SettingsPage = () => {
         <Page>
             <VStack gap="16">
                 <Text title={t('Настройки пользователя')} />
+                <HStack gap="8">
+                    <ThemeSwitcher />
+                    <Text title={t('Изменить цветовую схему')} size="s" />
+                </HStack>
             </VStack>
         </Page>
     );
