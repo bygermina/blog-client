73622083af9afac4327542573a3e349aa3c0d4da
Xenia.Liubachka <bygermina@gmail.com>
2025-03-17 23:20:30 +0100

    fix language

--- src/features/LangSwitcher/ui/LangSwitcher/LangSwitcher.tsx
+++ src/features/LangSwitcher/ui/LangSwitcher/LangSwitcher.tsx
@@ -12,7 +12,8 @@ export const LangSwitcher = memo(({ className, short }: Props) => {
     const { t, i18n } = useTranslation();
 
     const toggle = async () => {
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+        console.log(t(short ? 'Короткий язык' : 'Язык'));
+        await i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
     };
 
     return (
