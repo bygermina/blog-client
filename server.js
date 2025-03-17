7e45e571a690f881e5a7a1ae1c79cd2c6780067b
Xenia.Liubachka <bygermina@gmail.com>
2025-03-18 00:28:36 +0100

    fix path to locales folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use('public/locales', express.static(path.join(__dirname, 'locales')));
+app.use('/locales', express.static(path.join(__dirname, 'public', 'locales')));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
